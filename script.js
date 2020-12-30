6063f7e5fd250c8f4dc18543ed0face133960eda
Seth Frady <sethfrady@outlook.com>
2020-12-30 18:03:46 -0500

    Completed website for client

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrolling-active', windowPosition);
+});
